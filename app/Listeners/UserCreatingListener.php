55f4acec6bf8afac59d683f693accff4742bc19d
stalone01 <rasoamampioninastalone@gmail.com>
2024-04-29 16:14:31 +0300

    feat: creation de listener

---
+++ app/Listeners/UserCreatingListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\=UserCreatingEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class UserCreatingListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(=UserCreatingEvent $event): void
+    {
+        //
+    }
+}
