da0cdd6fd5c11fca5dcdba178c432d54590e7637
stalone01 <rasoamampioninastalone@gmail.com>
2024-04-29 15:36:57 +0300

    feat: installation de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\PlaceholderAPIController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[PlaceholderAPIController::class, 'index']);
+Route::view('/', 'welcome');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
